e68e4f51f381b2705110a2c68a7721fb49fd5be1
michelle <michelleadonario@yahoo.com.br>
2021-10-25 22:18:34 -0300

    2510

---
+++ 2510.js
@@ -0,0 +1,10 @@
+function  tabuada ( n ) {
+    for  ( var  i  =  0 ;  i <= 10 ;  i ++ ) {
+        var  linha  =  n  +  "x"  + i +  "="  + n  * i ;
+        console . log ( linha ) ;
+    }
+}
+for  ( var  i  = 2 ;  i  <=  2 ;  i ++ ) {
+    tabuada ( i ) ;
+    console . log ( "" ) ;
+}
